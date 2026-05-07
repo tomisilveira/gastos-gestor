bf75e0b7893601648108263c6b3b9e881657bf4f
Tomás Silveira <silveirat01@gmail.com>
2026-05-07 09:11:55 -0300

    Agregar supabaseClient y dependencia de Supabase

--- src/superbaseClient.js
+++ src/superbaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+// Usar variables de entorno o valores directos
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://TU_PROYECTO.supabase.co'
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'TU_ANON_KEY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
